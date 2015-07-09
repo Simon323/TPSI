965448a34028b194576f9e5a8c63b7501e42852f
Szymon <szmiekus@gmail.com>
2015-07-09 12:47:39 +0200

    Style poprawki

--- Lab4/REST/REST.Site/SPA/controller.js
+++ Lab4/REST/REST.Site/SPA/controller.js
@@ -59,3 +59,9 @@ app.controller('detailsController', function ($scope, $http, $routeParams) {
         isFirstDisabled: false
     };
 });
+
+app.filter("jsDate", function () {
+    return function (x) {
+        return new Date(parseInt(x.substr(6)));
+    };
+});
